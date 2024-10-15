2d5b4d007f66a1028d3bf395205a964e118490f3
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2024-10-15 11:11:04 -0700

    finalize hw-08 task

---
+++ js/gallery.js
@@ -0,0 +1,31 @@
+import images from "./imgData.js";
+
+const imgContainer = document.querySelector(".gallery");
+
+images.forEach((image) => {
+  const imgElement = `<li class="gallery-item" style="cursor: url('../assets/curserImg/cursor.png'), auto !important">
+  <a class="gallery-link" href=${image.original}>
+    <img
+      class="gallery-image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+  </a>
+</li>`;
+  imgContainer.innerHTML += imgElement;
+});
+
+imgContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const selectedImg = e.target;
+
+  if (selectedImg.classList.contains("gallery-image")) {
+    const largeImageURL = selectedImg.dataset.source;
+    const lightbox = basicLightbox.create(`
+      <img src="${largeImageURL}" alt="${selectedImg.alt}" style="border: 9px solid black">
+    `);
+
+    lightbox.show();
+  }
+});
